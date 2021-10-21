03ac1d1ee39eb50b4251acba608dcd341a8a8fab
cabraldev <86741673+cabraldev@users.noreply.github.com>
2021-10-20 22:16:26 -0300

    Add files via upload

---
+++ React parte 1: Componentes reutilizáveis para sua webapp/src/formValidator.js
@@ -0,0 +1,12 @@
+import validador from 'validator';
+
+class FormValidator{
+  
+     valida(state){
+         console.log('validado');
+         return false;
+     }
+
+}
+
+export default FormValidator;

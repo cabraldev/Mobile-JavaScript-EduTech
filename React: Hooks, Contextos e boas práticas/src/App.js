594d3efbfe3f2c59f80c25584f864657b49d1323
cabraldev <86741673+cabraldev@users.noreply.github.com>
2021-12-04 10:30:50 -0300

    Add files via upload

---
+++ React: Hooks, Contextos e boas práticas/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import "fontsource-roboto";
+import { Container, Typography } from "@material-ui/core";
+
+import ValidacoesCadastro from "./contexts/ValidacoesCadastro";
+
+import { validarCPF, validarSenha } from "./models/cadastro";
+class App extends Component {
+  render() {
+    return (
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center">
+          Formulário de cadastro
+        </Typography>
+        <ValidacoesCadastro.Provider
+          value={{ cpf: validarCPF, senha: validarSenha, nome: validarSenha }}
+        >
+          <FormularioCadastro aoEnviar={aoEnviarForm} />
+        </ValidacoesCadastro.Provider>
+      </Container>
+    );
+  }
+}
+
+function aoEnviarForm(dados) {
+  console.log(dados);
+}
+
+export default App;

e31c6937e9327b8101546165d4cb97b826f385ff
cabraldev <86741673+cabraldev@users.noreply.github.com>
2021-12-07 08:39:17 -0300

    Create Home.jsx

---
+++ React Router: Navegação em uma SPA/src/paginas/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ListaCategorias from '../components/ListaCategorias'
+import ListaPost from '../components/ListaPost'
+
+const Home = () => {
+
+  return (
+    <main>
+      <div className="container">
+        <h2 className="titulo-pagina">Pet notícias</h2>
+      </div>
+      <ListaCategorias />
+      <ListaPost url={'/posts'} />
+    </main>
+    
+  )
+}
+
+export default Home
